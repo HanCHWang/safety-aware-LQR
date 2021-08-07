eec91f34c0b13e16ee5baa6d76c4ec684c928c31
HanWang99 <54691572+HanWang99@users.noreply.github.com>
2021-08-07 15:00:18 +0100

    fix all the bugs
    still need to add the bound on the controller

--- PrimalDual.m
+++ PrimalDual.m
@@ -1,7 +1,7 @@
 function [K,l,value]=PrimalDual(IniSafeLqr,ObConsArray,epsilon)
 k=2;
-alpha=0.01*1/k;
-lambda=zeros(size(IniSafeLqr.h,2),IniSafeLqr.n);
+alpha=0.1*1/k;
+lambda=ones(size(IniSafeLqr.h,2),IniSafeLqr.n);
 x=zeros(2,IniSafeLqr.n);
 x(:,1)=IniSafeLqr.x0;
 u=zeros(2,IniSafeLqr.n);
@@ -22,6 +22,18 @@ while abs(value(k)-value(k-1))>epsilon%terminal condition
     [K,l]=Control(IniSafeLqr,lambda,ObConsArray,[1 0;0 1]);
     for t=1:IniSafeLqr.n-1
         u(:,t)=(K{t}*x(:,t)+l(:,t));
+%         if u(1,t)>=0.3
+%             u(1,t)=0.3;
+%         elseif u(1,t)<=-0.3
+%             u(1,t)=-0.3;
+%         end
+%         
+%         if u(2,t)>0.3
+%             u(2,t)=0.3;
+%         elseif u(2,t)<-0.3
+%             u(2,t)=-0.3;
+%         end
+        
         x(:,t+1)=IniSafeLqr.A*x(:,t)+IniSafeLqr.stepsize*IniSafeLqr.B*u(:,t);
         for i=1:size(IniSafeLqr.h,2)
             lambda(i,t)=lambda(i,t)+alpha*ObConsArray(t).sign(i)*(x(:,t)'*ObConsArray(t).H{i}*x(:,t)+ObConsArray(t).c{i}'*x(:,t)+ObConsArray(t).d{i});
@@ -33,7 +45,7 @@ while abs(value(k)-value(k-1))>epsilon%terminal condition
     value(k+1)=LagranCost(IniSafeLqr,ObConsArray,lambda,K,l);
     k=k+1;
     alpha=0.01*1/k;
-    value(k)-value(k-1)
+%     value(k)-value(k-1)
 end
 value=value(k);
 end
--- main.m
+++ main.m
@@ -7,7 +7,7 @@ clear all
 %%
 %define quantities
 n=50;
-stepsize=0.1;
+stepsize=1;
 A=[1 0;
     0 1];
 B=[1 0;
@@ -18,7 +18,8 @@ Q=[1 0;
     0 1];
 R=[1 0;
     0 1];
-h={[5*rand,5*rand,3*rand],[2*rand,2*rand,3*rand],[7*rand,7*rand,3*rand]};%multiple circle
+% h={[5*rand,5*rand,3*rand],[2*rand,2*rand,3*rand],[7*rand,7*rand,3*rand]};%multiple circle
+h={[5,5,0.5]};
 flag=zeros(size(h,2));
 sign=zeros(size(h,2));
 x=zeros(2,n);%plane movement
@@ -33,11 +34,26 @@ IniSafeLqr=SafeLqr(n,A,B,C,D,Q,R,h,x(:,1),stepsize);
 [K,l]=Control(IniSafeLqr,lambda,ObConsArray,[1 0;0 1]);%u=Kx+l
 
 flagsum=0;
+u=zeros(2,IniSafeLqr.n);
 for t=1:n-1
-    ObConsArray(t)=ObCons(t,h,x(:,t),flag,sign);
+    ObConsArray(t).xt=x(:,t);
     flag=FeasiCheck(ObConsArray(t),0);
     sign=FeasiCheck(ObConsArray(t),1);
-    x(:,t+1)=A*x(:,t)+stepsize*B*(K{t}*x(:,t)+l(:,t));
+    ObConsArray(t).flag=flag;
+    ObConsArray(t).sign=sign;
+    u(:,t)=(K{t}*x(:,t)+l(:,t));
+    if u(1,t)>=0.3
+        u(1,t)=0.3;
+    elseif u(1,t)<=-0.3
+        u(1,t)=-0.3;
+    end
+    
+    if u(2,t)>0.3
+        u(2,t)=0.3;
+    elseif u(2,t)<-0.3
+        u(2,t)=-0.3;
+    end
+    x(:,t+1)=A*x(:,t)+stepsize*B*u(:,t);
     flagsum=flagsum+sum(flag);
 end
 
@@ -45,29 +61,47 @@ value(1)=100000;
 k=2;
 while flagsum>0
     flagsum=0;
+    signsum=0;
     [H,c,d]=Convexification(ObConsArray);
     for t=1:n
         flag=FeasiCheck(ObConsArray(t),0);
+        flagsum=flagsum+flag;
         sign=FeasiCheck(ObConsArray(t),1);
+        signsum=signsum+sign;
         ObConsArray(t)=ObCons(t,h,x(:,t),flag,sign,{H(:,t),c(:,t),d(:,t)});
-        flagsum=flagsum+sum(flag);
     end
     [K,l,value(2)]=PrimalDual(IniSafeLqr,ObConsArray,epsilon);
-    while value(k)<value(k-1)
-        value(k)-value(k-1)
-        [H,c,d]=Convexification(ObConsArray);
-        for t=1:n
-            ObConsArray(t).H=H(:,t);
-            ObConsArray(t).c=c(:,t);
-            ObConsArray(t).d=d(:,t);
-%             ObConsArray(t)=ObCons(t,h,x(:,t),flag,sign,{H(:,t),c(:,t),d(:,t)});
-        end
-        [K,l,value(k+1)]=PrimalDual(IniSafeLqr,ObConsArray,epsilon);
-        for t=1:n-1
-            x(:,t+1)=A*x(:,t)+stepsize*B*(K{t}*x(:,t)+l(:,t));
-        end
-
-        k=k+1;
+    %first test general cases without convex-to-concave
+    % %     while value(k)<value(k-1)
+    % % %         value(k)-value(k-1)
+    % %         [H,c,d]=Convexification(ObConsArray);
+    % %         for t=1:n
+    % %             ObConsArray(t).H=H(:,t);
+    % %             ObConsArray(t).c=c(:,t);
+    % %             ObConsArray(t).d=d(:,t);
+    % %             %             ObConsArray(t)=ObCons(t,h,x(:,t),flag,sign,{H(:,t),c(:,t),d(:,t)});
+    % %         end
+    % %         [K,l,value(k+1)]=PrimalDual(IniSafeLqr,ObConsArray,epsilon);
+    % %         for t=1:n-1
+    % %             x(:,t+1)=A*x(:,t)+stepsize*B*(K{t}*x(:,t)+l(:,t));
+    % %         end
+    % %
+    % %         k=k+1;
+    % %     end
+    %     flagsum=0;
+    %     signsum=0;
+    %     for t=1:n
+    %         flag=FeasiCheck(ObConsArray(t),0);
+    %         flagsum=flagsum+flag;
+    %         sign=FeasiCheck(ObConsArray(t),1);
+    %         signsum=signsum+sign;
+    %         ObConsArray(t)=ObCons(t,h,x(:,t),flag,sign,{H(:,t),c(:,t),d(:,t)});
+    %     end
+    %     signsum
+    %     flagsum
+    for t=1:n-1
+        x(:,t+1)=A*x(:,t)+stepsize*B*(K{t}*x(:,t)+l(:,t));
+        ObConsArray(t).xt=x(:,t);
     end
     value(1)=100000;
     k=2;
